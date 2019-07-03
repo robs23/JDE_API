f2d52ce368b84989aac2068e462501f6e8e1fb7d
robert roszak <robert.roszak@outlook.com>
2019-07-03 11:37:55 +0200

    Migrating backend to local network

--- JDE_API/Global.asax.cs
+++ JDE_API/Global.asax.cs
@@ -15,6 +15,7 @@ namespace JDE_API
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
